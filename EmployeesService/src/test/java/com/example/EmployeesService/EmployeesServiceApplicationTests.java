2761222af0993fdcdda500d53d25132aaeaab805
Piyush Singh <Piyush.Singh@utc.com>
2020-12-20 22:03:55 +0530

    added service and instances

---
+++ EmployeesService/src/test/java/com/example/EmployeesService/EmployeesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
